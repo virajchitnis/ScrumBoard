366058fc9ab8edf984254535771d37538db7498a
Viraj Chitnis <chitnisviraj@gmail.com>
2015-01-24 09:06:47 -0500

    Fixed board route problem
    Fixed a problem which prevented the correct pages from showing up because of a misconfiguration of routes.

--- routes/board.js
+++ routes/board.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET board page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Capstone II Scrum Board' });
+  res.render('board', { title: 'Board' });
 });
 
 module.exports = router;
