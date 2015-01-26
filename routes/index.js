2a2ac424b7970971e17118a975a364a6fdb02638
Viraj Chitnis <chitnisviraj@gmail.com>
2015-01-26 15:00:03 -0500

    Modified the main page header
    The main page header has been redesigned.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Capstone II Scrum Board' });
+  res.render('index', { title: 'Capstone II Scrum Boards' });
 });
 
 module.exports = router;
