979dd53953bf9a251762effaad62e6a854fecfed
Viraj Chitnis <chitnisviraj@gmail.com>
2015-01-27 13:23:03 -0500

    The main page now uses REST api
    The teams on the main page are now loaded from MongoDB using REST api.
    Closes #14.

--- public/javascripts/index_app.js
+++ public/javascripts/index_app.js
@@ -1,16 +1,54 @@
 var app = angular.module('app', []);
 
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+	function resetNewTeamForm() {
+		$scope.teamName = "";
+		$scope.teamMembers = "";
+		$scope.teamDescription = "";
+		$scope.teamSecret = "";
+		$scope.teamBacklog = false;
+		$scope.teamCompleted = false;
+		$scope.teamSprints = 3;
+	}
+	
+	resetNewTeamForm();
+	
 	$http.get('./data').success(function(data) {
 		$scope.teams = data;
 	});
-	$scope.clickNavLink = function(elementID) {
-		var elementOffset = $("#" + elementID).offset().top;
-		$('html, body').animate({
-			scrollTop: (elementOffset - (10 + $("#contents-nav-fixed").height()))
-		}, 1000);
+	
+	$scope.addModalClosed = function() {
+		// Add new team to MongoDB database.
+		
+		if (($scope.teamName != "") && ($scope.teamMembers != "") && ($scope.teamDescription != "") && ($scope.teamSecret != "")) {
+			var Team = {
+				name: $scope.teamName,
+				members: $scope.teamMembers,
+				description: $scope.teamDescription,
+				secret: $scope.teamSecret,
+				backlog: $scope.teamBacklog,
+				completed: $scope.teamCompleted,
+				sprints: $scope.teamSprints
+			};
+		
+			$http.post('./data/teams', Team).
+			success(function(data, status, headers, config) {
+				$('#new-team-modal').modal('hide');
+				resetNewTeamForm();
+		
+				$http.get('./data').success(function(data) {
+					$scope.teams = data;
+				});
+			}).
+			error(function(data, status, headers, config) {
+				// called asynchronously if an error occurs
+				// or server returns response with an error status.
+			});
+		}
 	};
-	$scope.scrollToTop = function() {
-		$('html, body').animate({scrollTop: '0px'}, 1000);
-	}
+	
+	$scope.openTeamBoard = function(id) {
+		// Use this function to open the board and pass the selected team's id.
+		console.log(id);
+	};
 }]);
