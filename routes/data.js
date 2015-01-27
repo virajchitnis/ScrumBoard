53c0e639861869fdbb0672eaa9a5ccc52f18aa4e
Viraj Chitnis <chitnisviraj@gmail.com>
2015-01-26 19:49:53 -0500

    Some basic json data
    Some basic json data has been added to the data route in Nodejs.

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET all data listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var alldata = [
+		{
+			"teamName": "Faculty Affairs Application System",
+			"teamMembers": "John Curran, Brandon Bobo, Nicole Maalouf & Viraj Chitnis",
+			"teamDescription": "Database system for the Faculty Affairs Office at Temple University.",
+			"teamSecret": "xyzxyzxyz",
+			"teamBacklog": true,
+			"teamCompleted": true,
+			"teamSprintNums": 3,
+			"scrumItems": [
+				{
+					"itemSection": "UI",
+					"itemDetails": "Add captcha to the forms.",
+					"itemAssignee": "Viraj Chitnis",
+					"itemSprintNum": "1",
+					"itemColor": "blue"
+				},
+				{
+					"itemSection": "Back-End",
+					"itemDetails": "Make stored procedures.",
+					"itemAssignee": "Nicole Maalouf",
+					"itemSprintNum": "1",
+					"itemColor": "pink"
+				}
+			]
+		}
+	
+	];
+	res.send(alldata);
 });
 
 module.exports = router;
