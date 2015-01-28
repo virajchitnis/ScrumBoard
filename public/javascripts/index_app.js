32e3b128f5c9eb3bc3f2c7d81591b977e82a9dbb
Viraj Chitnis <chitnisviraj@gmail.com>
2015-01-28 16:01:03 -0500

    Team settings can only be modified with secret
    The team settings editing modal requires the entry of the correct team secret for modifying anything.
    Closes #4.

--- public/javascripts/index_app.js
+++ public/javascripts/index_app.js
@@ -1,12 +1,16 @@
 var app = angular.module('app', []);
 
 app.controller('MainCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+	$scope.displaySecretError = { display: 'none' };
+	
 	function resetTeamForm() {
 		$scope.teamId = "";
 		$scope.teamName = "";
 		$scope.teamMembers = "";
 		$scope.teamDescription = "";
 		$scope.teamSecret = "";
+		$scope.teamOldSecret = "";
+		$scope.teamNewSecret = "";
 		$scope.teamBacklog = false;
 		$scope.teamCompleted = false;
 		$scope.teamSprints = 3;
@@ -51,31 +55,46 @@ app.controller('MainCtrl', ['$scope', '$http', '$sce', function($scope, $http, $
 	$scope.editModalClosed = function() {
 		// Update existing team.
 		
-		if (($scope.teamName != "") && ($scope.teamMembers != "") && ($scope.teamDescription != "") && ($scope.teamSecret != "")) {
-			var Team = {
-				id: $scope.teamId,
-				name: $scope.teamName,
-				members: $scope.teamMembers,
-				description: $scope.teamDescription,
-				secret: $scope.teamSecret,
-				backlog: $scope.teamBacklog,
-				completed: $scope.teamCompleted,
-				sprints: $scope.teamSprints
-			};
+		if ($scope.teamOldSecret == $scope.teamSecret) {
+			if (($scope.teamName != "") && ($scope.teamMembers != "") && ($scope.teamDescription != "")) {
+				if ($scope.teamNewSecret != "") {
+					$scope.teamSecret = $scope.teamNewSecret;
+				}
+				
+				var Team = {
+					id: $scope.teamId,
+					name: $scope.teamName,
+					members: $scope.teamMembers,
+					description: $scope.teamDescription,
+					secret: $scope.teamSecret,
+					backlog: $scope.teamBacklog,
+					completed: $scope.teamCompleted,
+					sprints: $scope.teamSprints
+				};
 		
-			$http.put('./data/teams/' + $scope.teamId, Team).
-			success(function(data, status, headers, config) {
-				$('#edit-team-modal').modal('hide');
-				resetTeamForm();
+				$http.put('./data/teams/' + $scope.teamId, Team).
+				success(function(data, status, headers, config) {
+					$('#edit-team-modal').modal('hide');
+					$scope.secretError = "";
+					$scope.displaySecretError = { display: 'none' };
+					resetTeamForm();
 		
-				$http.get('./data').success(function(data) {
-					$scope.teams = data;
+					$http.get('./data').success(function(data) {
+						$scope.teams = data;
+					});
+				}).
+				error(function(data, status, headers, config) {
+					// called asynchronously if an error occurs
+					// or server returns response with an error status.
 				});
-			}).
-			error(function(data, status, headers, config) {
-				// called asynchronously if an error occurs
-				// or server returns response with an error status.
-			});
+			}
+		}
+		else {
+			if ($scope.teamOldSecret == "")
+				$scope.secretError = "Please enter the team secret, you can only modify team settings if you enter the correct team secret.";
+			else
+				$scope.secretError = "Incorrect secret, please try again!";
+			$scope.displaySecretError = { display: 'block' };
 		}
 	};
 	
@@ -105,9 +124,14 @@ app.controller('MainCtrl', ['$scope', '$http', '$sce', function($scope, $http, $
 		$scope.teamMembers = selectedTeam.members;
 		$scope.teamDescription = selectedTeam.description;
 		$scope.teamSecret = selectedTeam.secret;
+		$scope.teamOldSecret = "";
+		$scope.teamNewSecret = "";
 		$scope.teamBacklog = selectedTeam.backlog;
 		$scope.teamCompleted = selectedTeam.completed;
 		$scope.teamSprints = selectedTeam.sprints;
+		
+		$scope.secretError = "";
+		$scope.displaySecretError = { display: 'none' };
 		
 		$('#edit-team-modal').modal('show');
 	};
